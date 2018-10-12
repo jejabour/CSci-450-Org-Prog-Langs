4394ad0b4c2f2264588bb81f63a78708ec780114
Joseph <jejabour@gmail.com>
2018-10-12 11:41:48 -0500

    Added comments
    I added comments before the functions

--- Assignment 2/WWMPOP.hs
+++ Assignment 2/WWMPOP.hs
@@ -4,17 +4,22 @@ import Data.Fixed
 -- lineWidth :: Int
 -- lineWidth = 34
 
-
+-- Centi comes from the import, and makes it so it always
+-- has two decimal points
 type Price = Centi
 type Name = String
 type CartPrices = [(Name, Price)]
 
+-- Divides the given number by 100 to make cents out
+-- of the given number
 formatDollars :: Price -> String
 formatDollars x = show (x / 100)
 
+-- Returns the length of the Name
 getLengthName :: Name -> Int
 getLengthName n = length n
 
+-- Returns the length of the Price
 getLengthPrice :: Price -> Int
 getLengthPrice p = 
     let 
@@ -23,6 +28,9 @@ getLengthPrice p =
     in 
         id lengthPrice
 
+-- Returns the name of the item, follwed by a number of "."'s
+-- follwed by the price, where the number of characters is always
+-- 34
 formatLine :: (Name, Price) -> String
 formatLine (ns, p) = 
     let 
@@ -36,11 +44,13 @@ formatLine (ns, p) =
 
 
 -- No idea if this is working
+-- Creates a list of all the items using formatLine
 formatLines :: CartPrices -> String
 formatLines [] = []
 formatLines ((ns, p):nss) = formatLine(ns, p)
 
-
+-- No idea if this is working
+-- Sifts through the formatLines to find the total of each Price
 calcSubtotal :: CartPrices -> Price
 calcSubtotal ((ns, p):nss) = p + calcSubtotal(nss)
 
