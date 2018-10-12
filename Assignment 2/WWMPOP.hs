9d9d9d752c180c84cbdf3bad485fb3c88c0f648f
Joseph <jejabour@gmail.com>
2018-10-12 11:34:02 -0500

    Added functions for formatLine
    Added more functions for formatLine to use

--- Assignment 2/WWMPOP.hs
+++ Assignment 2/WWMPOP.hs
@@ -12,27 +12,35 @@ type CartPrices = [(Name, Price)]
 formatDollars :: Price -> String
 formatDollars x = show (x / 100)
 
+getLengthName :: Name -> Int
+getLengthName n = length n
 
+getLengthPrice :: Price -> Int
+getLengthPrice p = 
+    let 
+        getFormatPrice = formatDollars p
+        lengthPrice = length getFormatPrice
+    in 
+        id lengthPrice
 
 formatLine :: (Name, Price) -> String
 formatLine (ns, p) = 
     let 
-        lengthName = length ns
-        priceF = formatDollars p
-        lengthPrice = length priceF
+        priceFormat = formatDollars p
+        lengthName = getLengthName ns
+        lengthPrice = getLengthPrice p
         amountPeriod = 34 - (lengthName + lengthPrice)
         printR = concat(replicate amountPeriod ".")
     in
-         ns ++ printR ++ priceF ++ "\n"
+         ns ++ printR ++ priceFormat ++ "\n"
 
+
+-- No idea if this is working
 formatLines :: CartPrices -> String
 formatLines [] = []
 formatLines ((ns, p):nss) = formatLine(ns, p)
-    -- let
-    --     first = formatLine((ns, p))
-    --     rest = formatLines(xss)
-    -- in
-    --     first ++ rest
 
-    
+
+calcSubtotal :: CartPrices -> Price
+calcSubtotal ((ns, p):nss) = p + calcSubtotal(nss)
 
